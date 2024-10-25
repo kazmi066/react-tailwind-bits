c9ab003665a4b60823a86c978636ad7cd5fcb192
kazmi066 <awaisabbas47@gmail.com>
2024-10-25 21:58:18 +0500

    Add more navbar variants

--- src/components/navbars/v7/index.tsx
+++ src/components/navbars/v7/index.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
-export const Navbar5 = () => {
+export const Navbar7 = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <header className="bg-gray-800 my-2 w-full">
+    <header className="bg-gray-800 my-2 w-full bg-gradient-to-r from-fuchsia-500 to-cyan-500">
       <div className="mx-auto max-w-[1500px] p-3">
         <div className="flex items-center justify-between">
           {/* Left Panel */}
@@ -15,12 +15,16 @@ export const Navbar5 = () => {
               src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
               alt="Your Company"
             />
+          </div>
+
+          {/* Right Panel */}
+          <div className="flex items-center gap-5">
             <nav className="right-panel">
               <ul className="m-0 md:flex items-center gap-x-5 hidden list-none">
                 <li>
                   <a
                     href="#"
-                    className="underline text-white hover:no-underline"
+                    className="text-white no-underline hover:no-underline hover:text-gray-200"
                     aria-current="page"
                   >
                     About
@@ -29,7 +33,7 @@ export const Navbar5 = () => {
                 <li>
                   <a
                     href="#"
-                    className="underline text-white hover:no-underline"
+                    className="text-white no-underline hover:no-underline hover:text-gray-200"
                     aria-current="page"
                   >
                     Products
@@ -38,67 +42,23 @@ export const Navbar5 = () => {
                 <li>
                   <a
                     href="#"
-                    className="underline text-white hover:no-underline"
+                    className="text-white no-underline hover:no-underline hover:text-gray-200"
                     aria-current="page"
                   >
                     Demo
                   </a>
                 </li>
+                <li>
+                  <a
+                    href="#"
+                    className="rounded-md bg-white px-3 py-2 text-sm font-medium text-black hover:no-underline hover:bg-black hover:text-white"
+                    aria-current="page"
+                  >
+                    Sign In
+                  </a>
+                </li>
               </ul>
             </nav>
-          </div>
-
-          {/* Right Panel */}
-          <div className="flex items-center gap-5">
-            <div className="relative flex">
-              <img
-                className="w-10 h-10 rounded-full border-2 border-solid border-gray-400 cursor-pointer"
-                src="https://pagedone.io/asset/uploads/1704275541.png"
-                alt="Bordered avatar"
-                onClick={() => setIsMenuOpen(!isMenuOpen)}
-              />
-              <div
-                className={`w-80 inline-block absolute -right-[200%] md:right-1/2 top-full z-10 bg-white rounded-xl shadow-md text-left transition-all ${
-                  isMenuOpen
-                    ? "visible opacity-100 pointer-events-auto"
-                    : "invisible opacity-0 pointer-events-none"
-                }`}
-              >
-                <div className="p-3 border-b border-gray-200 flex items-center justify-between">
-                  <div className="flex items-center gap-3">
-                    <img
-                      src="https://pagedone.io/asset/uploads/1715323614.png"
-                      alt="image"
-                      className="w-12 h-12 rounded-full"
-                    />
-                    <div className="block">
-                      <h5 className="text-sm text-gray-900 font-medium m-0">John Doe</h5>
-                      <span className="text-sm text-gray-500 font-normal m-0">@johndoe12</span>
-                    </div>
-                  </div>
-                </div>
-                <ul className="m-0 px-0 space-y-[1px] list-none">
-                  <li>
-                    <a
-                      href="#"
-                      className="block p-3 bg-gray-900 text-sm font-medium text-white hover:no-underline"
-                      aria-current="page"
-                    >
-                      Dashboard
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block bg-gray-900 p-3 text-sm font-medium text-white hover:no-underline"
-                      aria-current="page"
-                    >
-                      Account Settings
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
 
             {/* Mobile Menu Button */}
             <button
@@ -138,7 +98,7 @@ export const Navbar5 = () => {
             <li>
               <a
                 href="#"
-                className="block rounded-md bg-gray-900 p-3 text-sm font-medium text-white hover:no-underline"
+                className="block rounded-md bg-slate-200 p-3 text-sm font-medium text-black hover:text-gray-500 hover:no-underline"
                 aria-current="page"
               >
                 About
@@ -147,7 +107,7 @@ export const Navbar5 = () => {
             <li>
               <a
                 href="#"
-                className="block rounded-md bg-gray-900 p-3 text-sm font-medium text-white hover:no-underline"
+                className="block rounded-md bg-slate-200 p-3 text-sm font-medium text-black hover:text-gray-500 hover:no-underline"
                 aria-current="page"
               >
                 Products
@@ -156,10 +116,19 @@ export const Navbar5 = () => {
             <li>
               <a
                 href="#"
-                className="block rounded-md bg-gray-900 p-3 text-sm font-medium text-white hover:no-underline"
+                className="block rounded-md bg-slate-200 p-3 text-sm font-medium text-black hover:text-gray-500 hover:no-underline"
                 aria-current="page"
               >
                 Demo
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block rounded-md bg-black p-3 text-sm font-medium text-white hover:no-underline hover:bg-white hover:text-black"
+                aria-current="page"
+              >
+                Sign In
               </a>
             </li>
           </ul>
